6bc22ea4745e6d2063a4bec5336e4f2180ecd9cd
braintuner <niyamsbhat@gmail.com>
2020-06-12 10:48:11 +0800

    Add files via upload

---
+++ dogyears.js
@@ -0,0 +1,15 @@
+//Dogs mature at a faster rate than human beings. We often say a dog’s age can be calculated in “dog years” to account for their growth compared to a human of the same age. In some ways we could say, time moves quickly for dogs — 8 years in a human’s life equates to 45 years in a dog’s life. How old would you be if you were a dog?
+
+Here’s how you convert your age from “human years” to “dog years”:
+var myAge=27;
+var earlyYears=2;
+earlyYears*=10.5;
+myAge-=2;
+var laterYears=myAge;
+laterYears*=4;
+console.log(earlyYears);
+console.log(laterYears);
+var myAgeInDogYears=earlyYears+laterYears;
+var myName='Niyam.toLowerCase()';
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
